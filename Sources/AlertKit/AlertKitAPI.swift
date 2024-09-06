8e25b5376d694fb0a878a67488b4d6988dff39d7
tiandt <hstdt@163.com>
2024-09-06 17:41:12 +0800

    pref: add window param

--- Sources/AlertKit/AlertKitAPI.swift
+++ Sources/AlertKit/AlertKitAPI.swift
@@ -2,23 +2,23 @@ import UIKit
 
 public enum AlertKitAPI {
     
-    public static func present(view: AlertViewProtocol, completion: @escaping ()->Void = {}) {
-        guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
+    public static func present(view: AlertViewProtocol, window: UIWindow? = nil, completion: @escaping ()->Void = {}) {
+        guard let window = window ?? UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
         view.present(on: window, completion: completion)
     }
     
-    public static func present(title: String? = nil, subtitle: String? = nil, icon: AlertIcon? = nil, style: AlertViewStyle, haptic: AlertHaptic? = nil) {
+    public static func present(title: String? = nil, subtitle: String? = nil, icon: AlertIcon? = nil, style: AlertViewStyle, haptic: AlertHaptic? = nil, window: UIWindow? = nil) {
         switch style {
         #if os(iOS)
         case .iOS16AppleMusic:
-            guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
+            guard let window = window ?? UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
             let view = AlertAppleMusic16View(title: title, subtitle: subtitle, icon: icon)
             view.haptic = haptic
             view.present(on: window)
         #endif
         #if os(iOS) || os(visionOS)
         case .iOS17AppleMusic:
-            guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
+            guard let window = window ?? UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
             let view = AlertAppleMusic17View(title: title, subtitle: subtitle, icon: icon)
             view.haptic = haptic
             view.present(on: window)
